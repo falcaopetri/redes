3bbfcffa8c6f2920ec98401d58c2cf66e4a11271
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-11-26 09:41:22 -0800

    Webserver processa HTML Form e gera um dicionario com os dados

--- projeto1/server/webserver.py
+++ projeto1/server/webserver.py
@@ -1,6 +1,39 @@
-#!/usr/bin/env python
-import cgitb
+#!/usr/bin/env python3
+import cgi	# handling of user input in CGI scripts	
+import cgitb	# displays nice tracebacks when errors occurs
 
-cgitb.enable()    
+import backend
+
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+class Command:
+	def __init__(self, cmd, params):
+		self.cmd = cmd
+
+		if params is None:
+			params = ""
+
+		self.params = params
+
+	def __repr__(self):
+		return self.cmd + ": " + self.params
+
+
+def getCommands(form, id):
+	cmds = form.getlist(id)
+	return [ Command(cmd, form.getvalue(id + "_" + cmd)) for cmd in cmds ]
+
+
+machine_ids = [ "maq" + str(i) for i in range(1, 4) ]
+commands = {}
+for id in machine_ids:
+	commands[id] = getCommands(form, id)
+
+response = backend.process(commands)
+
 print("Content-Type: text/html;charset=utf-8\r\n\r\n")
-print("Hello World!")
+print(commands)
+print("</br>")
+print(response)
