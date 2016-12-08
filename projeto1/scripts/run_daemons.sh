08e025056d7e3769d38097f24680c06d0eaa6a3f
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-12-08 14:34:21 -0800

    Minor update - gerenciamento de logging

--- projeto1/scripts/run_daemons.sh
+++ projeto1/scripts/run_daemons.sh
@@ -1,5 +1,9 @@
 BASEDIR="/home/aluno/redes/projeto1"
 
+# Kills all running daemons
+pkill -f daemon.py
+rm /tmp/daemon*
+
 $BASEDIR/slave/daemon.py 5000 &
 $BASEDIR/slave/daemon.py 5001 &
 $BASEDIR/slave/daemon.py 5002 &
